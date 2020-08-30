29229245dd5c1bd1f3d58e41edf14475fa0d474d
Harman Singh <harman.singh@hotmail.co.nz>
2020-08-30 22:08:26 +1200

    Switch to vue2.x project setup with basic leaflet map working

--- app/parking-finder/src/main.js
+++ app/parking-finder/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import 'leaflet/dist/leaflet.css';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
